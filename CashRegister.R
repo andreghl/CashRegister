1d7c1780464ff429ff0c118ba07b200cdcc15c6e
PotatoSauce <andrelghonda@gmail.com>
2024-03-12 21:07:22 +0100

    Initial commit

---
+++ CashRegister.R
@@ -0,0 +1,32 @@
+# Cash Register
+
+denom <- c(
+  'FIVE HUNDRED' = 500,
+  'TWO HUNDRED' = 200,
+  'ONE HUNDRED' = 100,
+  'FIFTY' = 50,
+  'TWENTY' = 20,
+  'TEN' = 10,
+  'FIVE' = 5,
+  'TWO' = 2,
+  'ONE' = 1,
+  'FIFTY CENTS' = 0.5,
+  'TWENTY CENTS' = 0.2,
+  'TEN CENTS' = 0.1,
+  'FIVE CENTS' = 0.05,
+  'TWO CENTS' = 0.02,
+  'ONE CENTS' = 0.01
+)
+
+checkCash <- function(price, cash) {
+  
+  output <- NULL
+  change <- cash - price
+  
+  register <- function(account, currency) {
+    
+  }
+  
+}
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# CashRegister
+ A model to simulate the behavior of a cash register
